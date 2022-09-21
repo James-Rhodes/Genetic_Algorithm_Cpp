3d336e16819db6f42bda8dbb2c18f9f4d3e41b55
James Rhodes <james.rhodes@hotmail.com.au>
2022-09-21 22:56:26 +1000

    Made the premake file easier to work with my defining a function to declare example projects

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,31 @@
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+function CreateExampleProject(projName)
+   project (projName)
+      kind "ConsoleApp"
+      language "C++"
+      targetdir ("%{wks.location}/bin/" .. outputdir .. "/Examples/%{prj.name}")
+      objdir ("%{wks.location}/bin-int/" .. outputdir .. "/Examples/%{prj.name}")
+      location("%{wks.location}/Examples/%{prj.name}")
+
+      includedirs{
+         "%{wks.location}/Genetic_Algorithm_Cpp/include"
+      }
+
+      files { "Examples/%{prj.name}/**.h","Examples/%{prj.name}/**.hpp","Examples/%{prj.name}/**.cpp" }
+
+
+
+      filter "configurations:Debug"
+         defines { "DEBUG" }
+         symbols "On"
+         
+         filter "configurations:Release"
+         defines { "NDEBUG" }
+         optimize "On"
+end
+
 
 workspace "Genetic_Algorithm_Cpp"
    configurations { "Debug", "Release" }
@@ -30,53 +55,8 @@ project "Genetic_Algorithm_Cpp"
       defines { "NDEBUG" }
       optimize "On"
 
+CreateExampleProject("basic_optimisation")
+CreateExampleProject("setting_custom_selection_function")
 
-project "basic_optimisation"
-
-   kind "ConsoleApp"
-   language "C++"
-	targetdir ("%{wks.location}/bin/" .. outputdir .. "/Examples/%{prj.name}")
-	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/Examples/%{prj.name}")
-   location("%{wks.location}/Examples/%{prj.name}")
-
-   includedirs{
-      "%{wks.location}/Genetic_Algorithm_Cpp/include"
-   }
-
-   files { "Examples/%{prj.name}/**.h","Examples/%{prj.name}/**.hpp","Examples/%{prj.name}/**.cpp" }
-
-
-
-   filter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "On"
-      
-      filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "On"
-
-project "setting_custom_selection_function"
-
-      kind "ConsoleApp"
-      language "C++"
-      targetdir ("%{wks.location}/bin/" .. outputdir .. "/Examples/%{prj.name}")
-      objdir ("%{wks.location}/bin-int/" .. outputdir .. "/Examples/%{prj.name}")
-      location("%{wks.location}/Examples/%{prj.name}")
-   
-      includedirs{
-         "%{wks.location}/Genetic_Algorithm_Cpp/include"
-      }
-   
-      files { "Examples/%{prj.name}/**.h","Examples/%{prj.name}/**.hpp","Examples/%{prj.name}/**.cpp" }
-   
-   
-   
-      filter "configurations:Debug"
-         defines { "DEBUG" }
-         symbols "On"
-         
-         filter "configurations:Release"
-         defines { "NDEBUG" }
-         optimize "On"
    
 
